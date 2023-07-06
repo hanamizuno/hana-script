0df10e3c6f817d78700e7e06035fbf9442204b25
MIZUNO Kosuke <kosukenozumi@gmail.com>
2023-07-06 18:22:30 +0900

    Add transcribe_audio, fix type annotations

--- hana_script/transcription.py
+++ hana_script/transcription.py
@@ -1,10 +1,14 @@
 """Transcribe audio."""
 import os
+import tempfile
 from pathlib import Path
+from typing import cast
 
 import openai
 from dotenv import load_dotenv
+from pydub import AudioSegment
 
+# https://platform.openai.com/docs/api-reference/audio/create
 AUDIO_SUFFIXES = [
     "mp3",
     "mp4",
@@ -21,10 +25,23 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def transcribe_audio_file(audio_file_path: Path) -> str:
-    """Transcribe audio file using OpenAI Audio API."""
+    """Transcribe audio file using OpenAI Audio API.
+
+    https://platform.openai.com/docs/api-reference/audio/create
+    """
     if audio_file_path.suffix in AUDIO_SUFFIXES:
         raise ValueError("File type not supported.")
     with open(audio_file_path, "rb") as f:
-        response = openai.Audio.transcribe("whisper-1", file=f, language="ja")
+        response = openai.Audio.transcribe("whisper-1", file=f, language="ja", response_format="verbose_json")  # type: ignore  # noqa: E501
+        text = cast(str, response.text)  # type: ignore
 
-    return response.text
+    return text
+
+
+def transcribe_audio(audio: AudioSegment) -> str:
+    """Transcribe audio."""
+    with tempfile.NamedTemporaryFile(suffix=".wav") as f:
+        audio.export(f.name, format="wav")
+        text = transcribe_audio_file(Path(f.name))
+
+    return text
