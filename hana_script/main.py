f50a3e582b0a543b3fd3763d6e1bb92adb4b200b
MIZUNO Kosuke <kosukenozumi@gmail.com>
2023-07-06 18:22:58 +0900

    Add logger, extract_non_silent

--- hana_script/main.py
+++ hana_script/main.py
@@ -1,15 +1,38 @@
 """hana-script"""
+import sys
 from argparse import ArgumentParser
+from logging import DEBUG, Formatter, StreamHandler, getLogger
 from pathlib import Path
 
-from hana_script.transcription import transcribe_audio_file
+from hana_script.audio import extract_non_silent, load_audio
+from hana_script.transcription import transcribe_audio
+
+logger = getLogger(__name__)
+logger.setLevel(DEBUG)
+formatter = Formatter(
+    "[%(levelname)s]%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
+stream_handler = StreamHandler(sys.stdout)
+stream_handler.setLevel(DEBUG)
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
 
 
 def main(audio_file_path_str: str) -> None:
     """Main function."""
     audio_file_path = Path(audio_file_path_str)
-    transcription = transcribe_audio_file(audio_file_path)
-    print(transcription)
+    audio = load_audio(audio_file_path)
+    logger.info(f"Loaded {audio_file_path}")
+    logger.info(f"Duration: {audio.duration_seconds} seconds")
+
+    non_silent_chunks = extract_non_silent(audio)
+    logger.info(f"Found {len(non_silent_chunks)} non-silent chunks")
+
+    transcriptions = [
+        transcribe_audio(chunk) for chunk, _ in non_silent_chunks
+    ]
+    transcription = "\n".join(transcriptions)
+    logger.info(f"Transcription:\n{transcription}")
 
     output_file_path = audio_file_path.with_suffix(".txt")
     with open(output_file_path, "w") as f:
